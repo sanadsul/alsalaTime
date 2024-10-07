00e45264161e876465dd88c83947e9e24377187b
SanadSuleman <110310084+sanadsul@users.noreply.github.com>
2024-10-07 06:06:59 +0200

    Update Errorpage.jsx

--- pages/Errorpage.jsx
+++ pages/Errorpage.jsx
@@ -1,18 +1,17 @@
-
 const Errorpage = () => {
   return (
     <div className="bg-gray-100 text-gray-900 min-h-screen" dir="rtl">
-        {/* Header Section */}
-        <header 
-  className="bg-gradient-to-r from-teal-500 to-blue-700 bg-center bg-no-repeat text-white p-96  shadow-lg">
-          <div className="container mx-auto text-center">
-            <h1 className="text-4xl font-bold">الصفحة غير متــوفـرة</h1>
-            <div className="mt-12"></div>
-          </div>
-        </header>
-        <div/>
+      {/* Header Section */}
+      <header 
+        className="bg-gradient-to-r from-teal-500 to-blue-700 bg-center bg-no-repeat text-white p-96 shadow-lg">
+        <div className="container mx-auto text-center">
+          <h1 className="text-4xl font-bold">الصفحة غير متــوفـرة</h1>
+          <div className="mt-12"></div>
+        </div>
+      </header>
+      {/* Content Section */}
     </div>
-  )
+  );
 }
 
-export default Errorpage
+export default Errorpage;
