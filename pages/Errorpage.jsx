6e5822cfc9726dea4293642cac0cc30e852fa700
SanadSuleman <110310084+sanadsul@users.noreply.github.com>
2024-10-07 06:21:26 +0200

    Update Errorpage.jsx

--- pages/Errorpage.jsx
+++ pages/Errorpage.jsx
@@ -3,7 +3,7 @@ const Errorpage = () => {
     <div className="bg-gray-100 text-gray-900 min-h-screen" dir="rtl">
       {/* Header Section */}
       <header 
-        className="bg-gradient-to-r from-teal-500 to-blue-700 bg-center bg-no-repeat text-white justify-center items-center shadow-lg">
+        className="bg-gradient-to-r from-teal-500 to-blue-700 bg-center mx-auto my-auto text-white justify-center items-center shadow-lg">
         <div className="container justify-center text-center">
           <h1 className="text-4xl font-bold">الصفحة غير متــوفـرة</h1>
           <div className="mt-12"></div>
