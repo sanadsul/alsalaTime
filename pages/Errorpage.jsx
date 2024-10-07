63d3758667cbf1729a46548b8ebc2880c10eecdb
SanadSuleman <110310084+sanadsul@users.noreply.github.com>
2024-10-07 06:23:53 +0200

    Update Errorpage.jsx

--- pages/Errorpage.jsx
+++ pages/Errorpage.jsx
@@ -1,6 +1,6 @@
 const Errorpage = () => {
   return (
-    <div className="bg-gray-100 text-gray-900 min-h-screen text-center" dir="rtl">
+    <div className="bg-gray-100 text-gray-900 min-h-screen text-center justify-center items-center" dir="rtl">
       {/* Header Section */}
       <header 
         className="bg-gradient-to-r from-teal-500 to-blue-700 bg-center mx-auto my-auto text-white text-center justify-center items-center shadow-lg">
