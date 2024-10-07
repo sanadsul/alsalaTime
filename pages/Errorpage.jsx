7ae52c2d9075f5110da0895cb1f9f3230f96ddb3
SanadSuleman <110310084+sanadsul@users.noreply.github.com>
2024-10-07 06:19:27 +0200

    Update Errorpage.jsx

--- pages/Errorpage.jsx
+++ pages/Errorpage.jsx
@@ -4,7 +4,7 @@ const Errorpage = () => {
       {/* Header Section */}
       <header 
         className="bg-gradient-to-r from-teal-500 to-blue-700 bg-center bg-no-repeat text-white justify-center items-center shadow-lg">
-        <div className="container mx-auto text-center">
+        <div className="container justify-center text-center">
           <h1 className="text-4xl font-bold">الصفحة غير متــوفـرة</h1>
           <div className="mt-12"></div>
         </div>
